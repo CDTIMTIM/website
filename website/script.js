6ca6674f54c2041c95386bd597e940e09292c5fd
CDTIMTIM <164183236+CDTIMTIM@users.noreply.github.com>
2024-04-18 20:03:28 +0800

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,7 @@
+/* ======================================== typing animation ================================================ */
+var typed = new Typed(".Typing",{
+    String:["","Web Designer","Web Developer","Editor"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
